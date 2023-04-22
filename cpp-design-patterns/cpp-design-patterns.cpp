73dc5f38da1b1e9f1422cadaebfbcbd889f29afc
Eric Smith <ydrive65@yahoo.com>
2023-04-22 17:08:01 -0400

    Add project files.

---
+++ cpp-design-patterns/cpp-design-patterns.cpp
@@ -0,0 +1,12 @@
+﻿// cpp-design-patterns.cpp : Defines the entry point for the application.
+//
+
+#include "cpp-design-patterns.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
